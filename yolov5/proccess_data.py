963d4ac68f94b6c350701284060e67ede16fd5bd
cvmart-2786 <cvmart-2786@cvmart.net>
2020-08-22 12:40:48 +0800

    add files which are needed for train rat dataset

--- yolov5/proccess_data.py
+++ yolov5/proccess_data.py
@@ -10,17 +10,21 @@ if __name__=="__main__":
     names = glob.glob('*.jpg')
     names = [os.path.splitext(name)[0] for name in names]
     for name in names:
-        xml_path = osp.join(path, '{}.xml'.format(name))
+        xml_path = os.path.join(path, '{}.xml'.format(name))
         tree = ET.parse(xml_path)
         root = tree.getroot()
-        width = float(root.find('width').text)
-        height = float(root.find('height').text)
-        with open("project/train/src_repo/labels/" + name + '.txt','w') as f:
-            for obj in root.findall('object'):
-                bb_box = obj.find('bndbox')
-                x = (float(bb_box.find('xmin').text) + float(bb_box.find("xmax").text)) / (2 * width)
-                y = (float(bb_box.find('ymin').text) + float(bb_box.find("ymax").text)) / (2 * height)
-                w = (float(bb_box.find('xmax').text) - float(bb_box.find("xmin").text)) / width
-                h = (float(bb_box.find('ymax').text) - float(bb_box.find("ymin").text)) / height
-                f.write('0 ' + x +' '+y+' '+w+' '+h+"\n") 
+        size = root.find('size')
+        width = float(size.find('width').text)
+        height = float(size.find('height').text)
+        # if not os.path.exists("/project/train/src_repo/labels/" + name + '.txt'):
+        #     os.mkdir("/project/train/src_repo/labels/" + name + '.txt')
+        if root.findall('object') is not None:
+            with open("/project/train/src_repo/yolov5/rat_dataset/labels/" + name + '.txt','w') as f:
+                for obj in root.findall('object'):
+                    bb_box = obj.find('bndbox')
+                    x = (float(bb_box.find('xmin').text) + float(bb_box.find("xmax").text)) / (2 * width)
+                    y = (float(bb_box.find('ymin').text) + float(bb_box.find("ymax").text)) / (2 * height)
+                    w = (float(bb_box.find('xmax').text) - float(bb_box.find("xmin").text)) / width
+                    h = (float(bb_box.find('ymax').text) - float(bb_box.find("ymin").text)) / height
+                    f.write('0 ' + str(x) +' '+str(y)+' '+str(w)+' '+str(h)+"\n") 
             
